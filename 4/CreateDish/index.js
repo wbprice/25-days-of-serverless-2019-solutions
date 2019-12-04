0501d7e732b931bbe554a0c3febfdf5ae10cc39a
Blaine Price <1wbprice@gmail.com>
2019-12-04 11:18:22 -0500

    Start roughing out function

--- 4/CreateDish/index.js
+++ 4/CreateDish/index.js
@@ -1,16 +1,60 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+function validateInput(payload, callback) {
+    // Accepts a payload and callback function.
+    // Payload must include dish and name keys.
+    // If an error occurred, the callback is invoked with 
+    // an error message as the first argument
+    // If the payload is OK, the callback is invoked with
+    // no error message and a sanitized payload.
+
+    const dish = payload["dish"];
+    const name = payload["name"]
+
+    if (!name) {
+        callback("'name' is a required field'");
+    } else if (!dish) {
+        callback("'dish' is a required field'");
+    } else if (typeof dish != String) {
+        callback("'dish should be a string'");
+    } else if (typeof name != String) {
+        callback("'dish should be a string'");
+    } else {
+        callback(null, {
+            dish,
+            name
+        });
     }
-    else {
+}
+
+function createDish(payload, callback) {
+    // Noop
+    return callback(null, payload);
+}
+
+module.exports = async function (context, req) {
+
+    validateInput(req.body, (err, payload) => {
+
+        if (err) {
+            // Handle errors, exit early.
+            context.res = {
+                status: 400,
+                body: JSON.stringify({ "message": err })
+            };
+            return;
+        }
+
+
+    });
+
+    try {
+        // Check that payload is good. If so, persist it to storage.
+        const payload = validateInput(req.body);
         context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            status: 200,
+            body: JSON.stringify(payload)
         };
+
+    } catch (e) {
     }
 };
